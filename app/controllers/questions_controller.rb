b324a07c8faabca0b9a2abc42520b3c11be79b28
Nata-ly <nikifor1114@mail.ru>
2022-08-24 23:41:35 +0700

    Feat: Added attaching files to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -38,12 +38,12 @@ class QuestionsController < ApplicationController
   private
 
   def question
-    @question ||= Question.find(params[:id])
+    @question ||= Question.with_attached_files.find(params[:id])
   end
 
   helper_method :question
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, files: [])
   end
 end
