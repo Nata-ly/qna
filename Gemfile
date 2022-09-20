0fca054a25fee7c56e4f81fbe2db5f97cd8f33e5
Nata-ly <nikifor1114@mail.ru>
2022-09-20 21:52:02 +0700

    Add Github oauth

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'devise'
 gem 'jquery-rails'
 gem 'aws-sdk-s3', require: false
 gem 'cocoon'
+gem 'omniauth'
+gem 'omniauth-github'
+gem 'omniauth-rails_csrf_protection'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
 
   has_many :answers
   has_many :comment
+  has_many :authorizations
   has_many :questions
   has_one  :reward
   has_many :votes, dependent: :destroy
 
+  def self.find_for_oauth(auth)
+    FindForOauthService.new(auth).call
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
+  end
 end
