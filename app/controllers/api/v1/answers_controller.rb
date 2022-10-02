ca2d3aabfaf2e9cd1faf835e8383413fd4c23429
Nata-ly <nikifor1114@mail.ru>
2022-10-03 00:50:32 +0700

    Feat: Add separate answer api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -6,9 +6,17 @@ class Api::V1::AnswersController < Api::V1::BaseController
     render json: @answers
   end
 
+  def show
+    render json: answer, serializer: AnswerSeparateSerializer
+  end
+
   private
 
   def question
     @question ||= Question.find(params[:question_id])
+  end
+
+  def answer
+    @answer ||= Answer.find(params[:id])
   end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -40,4 +40,61 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/answers/:id' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:answer) { create(:answer, user: user, question: question) }
+    let!(:comments) { create_list(:comment, 2, commentable: answer, user: user) }
+    let!(:links) { create_list(:link, 2, linkable: answer) }
+
+    before do
+      answer.files.attach(
+        io: File.open(Rails.root.join('spec', 'rails_helper.rb')),
+        filename: 'rails_helper.rb'
+      )
+    end
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:access_token) {create(:access_token)}
+
+      before {get api_path, params: {access_token: access_token.token}, headers: headers}
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it_behaves_like 'Returns public fields' do
+        let(:attrs) { %w[id body user_id created_at updated_at] }
+        let(:resource_response) { json['answer'] }
+        let(:resource) { answer }
+      end
+
+      describe 'comments' do
+        it_behaves_like 'Returns list' do
+          let(:resource_response) { json['answer']['comments'] }
+          let(:resource) { comments }
+        end
+      end
+
+      describe 'files' do
+        it_behaves_like 'Returns list' do
+          let(:resource_response) { json['answer']['files'] }
+          let(:resource) { answer.files }
+        end
+      end
+
+      describe 'links' do
+        it_behaves_like 'Returns list' do
+          let(:resource_response) { json['answer']['links'] }
+          let(:resource) { links }
+        end
+      end
+    end
+  end
 end
