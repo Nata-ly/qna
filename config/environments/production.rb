38e8c3429273a2e6464cb81eb52e9f5dbb39c29a
Nata-ly <nikifor1114@mail.ru>
2022-12-15 22:47:40 +0700

    Change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: '5.159.103.28' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
